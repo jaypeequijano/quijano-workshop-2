4d3bf630487cba32599db75406fdd137e2e95450
jaypeequijano <35474664+jaypeequijano@users.noreply.github.com>
2018-02-03 13:08:28 +0800

    Update 3.php

--- 3.php
+++ 3.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title></title>
-		<link rel="stylesheet" type="text/css" href="../css/style.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 <label class="headers">Cisco Icons and Descriptions</label><br>
@@ -17,81 +17,81 @@
 				</tr>
 				<tr>
 					<td width="150px"><label class="iconName">Hub-PT</label></td>
-					<td><img src="../img/img16.jpg" height="50px" width="400px"></td>
+					<td><img src="img16.jpg" height="50px" width="400px"></td>
 					<td><p>The Hub-PT provides ten slots.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Repeater-PT</label></td>
-					<td><img src="../img/img17.jpg" height="50px" width="400px"></td>
+					<td><img src="img17.jpg" height="50px" width="400px"></td>
 					<td><p>The Repeater-PT provides two slots.<br>
 					<br>
 					The Repeater-PT supports the same modules that the Hub-PT supports.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">CoAxialSplitter-PT</label></td>
-					<td><img src="../img/img18.jpg" height="50px" width="400px"></td>
+					<td><img src="img18.jpg" height="50px" width="400px"></td>
 					<td><p>The CoAxialSplitter-PT does not support modules.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">AccessPoint-PT</label></td>
-					<td><img src="../img/img19.jpg" height="50px" width="400px"></td>
+					<td><img src="img19.jpg" height="50px" width="400px"></td>
 					<td><p>The AccessPoint-PT has a built-in antenna and provides one slot.<br>
 					<br>
 					The AccessPoint-PT supports the same modules that the Hub-PT supports.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">AccessPoint-PT-A</label></td>
-					<td><img src="../img/img19.jpg" height="50px" width="400px"></td>
+					<td><img src="img19.jpg" height="50px" width="400px"></td>
 					<td><p>The AccessPoint-PT has a built-in antenna and provides one slot.<br>
 					<br>
 					The AccessPoint-PT supports the same modules that the Hub-PT supports.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">AccessPoint-PT-N</label></td>
-					<td><img src="../img/img19.jpg" height="50px" width="400px"></td>
+					<td><img src="img19.jpg" height="50px" width="400px"></td>
 					<td><p>The AccessPoint-PT has a built-in antenna and provides one slot.<br>
 					<br>
 					The AccessPoint-PT supports the same modules that the Hub-PT supports.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">WRT300N</label></td>
-					<td><img src="../img/img20.jpg" height="50px" width="400px"></td>
+					<td><img src="img20.jpg" height="50px" width="400px"></td>
 					<td><p>The WRT300N wireless router has a built-in antenna and provides four fixed 10/100 (100BASE-TX) Ethernet ports and one fixed Internet port (also 100BASE-TX, typically for connection to cable and DSL modems).<br>
 					<br>
 					The WRT300N wireless router does not support modules.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Cloud-PT</label></td>
-					<td><img src="../img/img21.jpg" height="50px" width="400px"></td>
+					<td><img src="img21.jpg" height="50px" width="400px"></td>
 					<td><p>Although a cloud is not a single device, Packet Tracer gives you access to a representation of a cloud. It provides ten slots, a console port, and an auxiliary port.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">DSL-Modem-PT</label></td>
-					<td><img src="../img/img22.jpg" height="50px" width="400px"></td>
+					<td><img src="img22.jpg" height="50px" width="400px"></td>
 					<td><p>The DSL-Modem-PT provides one slot.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Cable-Modem-PT</label></td>
-					<td><img src="../img/img23.jpg" height="50px" width="400px"></td>
+					<td><img src="img23.jpg" height="50px" width="400px"></td>
 					<td><p>The Cable-Modem-PT provides one slot.<br>
 					<br>
 					The Cable-Modem-PT supports the same modules that the DSL-Modem-PT supports.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">ASA 5505</label></td>
-					<td><img src="../img/img24.jpg" height="50px" width="400px"></td>
+					<td><img src="img24.jpg" height="50px" width="400px"></td>
 					<td><p>TThe ASA 5505 is from Cisco's line of network security devices.<br>
 					<br>
 					It is a fixed-configuration that provides Ethernet connectivity. It has one available slot but it is currently not supported in this version of Packet Tracer.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Central Office Server</label></td>
-					<td><img src="../img/img25.png" height="50px" width="400px"></td>
+					<td><img src="img25.png" height="50px" width="400px"></td>
 					<td><p>The Central Office Server has six built-in coaxial ports and one FastEthernet port.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Cell Tower</label></td>
-					<td><img src="../img/img26.png" height="50px" width="400px"></td>
+					<td><img src="img26.png" height="50px" width="400px"></td>
 					<td><p>The Cell Tower has one slot. It supports coaxial and 3G/4G module.</p></td>
 				</tr>
 			</table>
