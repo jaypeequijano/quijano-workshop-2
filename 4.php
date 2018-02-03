b2e24f6096ae7070a81ccedd203879bbac90e0ca
jaypeequijano <35474664+jaypeequijano@users.noreply.github.com>
2018-02-03 13:03:21 +0800

    Add files via upload

---
+++ 4.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+<label class="headers">Cisco Icons and Descriptions</label><br>
+	
+		
+		
+		<label class="secondHeader">Cable Types</label>
+			<table class="iconTable">
+			<tr>
+					<td width="150px" id="tableHeader">Icon Name</td>
+					<td id="tableHeader2">Icon Image</td>
+					<td id="tableHeader3">Description</td>
+			</tr>
+			<tr>
+					<td><label class="iconName">RJ-45</label></td>
+					<td><img src="../img/27.jpg" height="100px" width="400px"></td>
+					<td><p>An 8-pin/8-position plug or jack is commonly used to connect computers onto Ethernet-based local area networks (LAN).<br>
+					<br>
+					Two wiring schemes–T568A and T568B–are used to terminate the twisted-pair cable onto the connector interface.</p></td>
+			</tr>
+			<tr>
+					<td><label class="iconName">Straight Through Cable</label></td>
+					<td><img src="../img/28.jpg" height="100px" width="400px"></td>
+					<td><p>is a type of twisted pair copper wire cable for local area network (LAN) use for which the RJ-45 connectors at each end have the same pinout (i.e., arrangement of conductors).</p></td>
+			</tr>
+			<tr>
+					<td><label class="iconName">Crossover Cable</label></td>
+					<td><img src="../img/29.jpg" height="200px" width="300px"></td>
+					<td><p>is a type of twisted pair copper wire cable for LANs (local area network) in which the wires on the cable are crossed over so that the receive signal pins on the RJ-45 connector on one end are connected to the transmit signal pins on the RJ-45 connector on the other end.</p></td>
+			</tr>
+			</table>
+</body>
+</html>
