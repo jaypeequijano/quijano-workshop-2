269a7d33dd62d3f45753d5d16d2bbd9b1a9cc132
jaypeequijano <35474664+jaypeequijano@users.noreply.github.com>
2018-02-03 13:06:07 +0800

    Update 1.php

--- 1.php
+++ 1.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title></title>
-		<link rel="stylesheet" type="text/css" href="../css/style.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 <label class="headers">Cisco Icons and Descriptions</label><br>
@@ -15,48 +15,48 @@
 				</tr>
 				<tr>
 					<td><label class="iconName">Router: 1841</label></td>
-					<td><img src="../img/img1.jpg" height="50px" width="400px"></td>
+					<td><img src="img1.jpg" height="50px" width="400px"></td>
 					<td><p>The Cisco 1841 Integrated Services Router provides two fixed 10/100 (100BASE-TX) Ethernet ports, two integrated High-Speed WAN Interface Card (HWIC) slots that are compatible with WAN Interface Card (WICs) and Voice/WAN Interface Cards (VWICs), and one internal Advanced Integration Module (AIM) slot.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Router: 1941</label></td>
-					<td><img src="../img/img2.jpg" height="50px" width="400px"></td>
+					<td><img src="img2.jpg" height="50px" width="400px"></td>
 					<td><p>The Cisco 1941 Integrated Services Router (ISR) provides 2 integrated 10/100/1000 Ethernet ports, 2 WAN Interface Card (WIC) slots and 1 Internal Services Module slot.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Router: 2620XM</label></td>
-					<td><img src="../img/img3.jpg" height="50px" width="400px"></td>
+					<td><img src="img3.jpg" height="50px" width="400px"></td>
 					<td><p>The Cisco 2620XM Multiservice Router provides a one-network module slot platform with one fixed 10/100 (100BASE-TX) Ethernet port, two integrated WAN Interface Card (WIC) slots, and one Advanced Integration Module (AIM) slot.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Router: 2621XM</label></td>
-					<td><img src="../img/img4.jpg" height="50px" width="400px"></td>
+					<td><img src="img4.jpg" height="50px" width="400px"></td>
 					<td><p>The Cisco 2621XM Multiservice Router provides a one-network module slot platform with two fixed 10/100 (100BASE-TX) Ethernet ports, two integrated WAN Interface Card (WIC) slots, and one Advanced Integration Module (AIM) slot.
 					The 2621XM supports the same modules that the 2620XM supports.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Router: 2811</label></td>
-					<td><img src="../img/img5.jpg" height="50px" width="400px"></td>
+					<td><img src="img5.jpg" height="50px" width="400px"></td>
 					<td><p>The Cisco 2811 Integrated Services Router provides one Enhanced Network-Module slot with two fixed 10/100 (100BASE-TX) Ethernet ports, four integrated High-Speed WAN Interface Card (HWIC) slots that are compatible with WAN Interface Card (WICs), Voice Interface Cards (VICs) and Voice/WAN Interface Cards (VWICs), and dual Advanced Integration Module (AIM) slots.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Router: 2901</label></td>
-					<td><img src="../img/img6.jpg" height="50px" width="400px"></td>
+					<td><img src="img6.jpg" height="50px" width="400px"></td>
 					<td><p>The Cisco 2901 Integrated Services Router (ISR) provides 2 integrated 10/100/1000 Ethernet ports, 4 enhanced high-speed WAN interface card (WIC) slots, 2 onboard digital signal processor (DSP) slots and 1 onboard Internal Service Module for application services.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Router: 2911</label></td>
-					<td><img src="../img/img7.jpg" height="50px" width="400px"></td>
+					<td><img src="img7.jpg" height="50px" width="400px"></td>
 					<td><p>The Cisco 2901 Integrated Services Router (ISR) provides 2 integrated 10/100/1000 Ethernet ports, 4 enhanced high-speed WAN interface card (WIC) slots, 2 onboard digital signal processor (DSP) slots and 1 onboard Internal Service Module for application services.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Router: 819</label></td>
-					<td><img src="../img/img8.png" height="50px" width="400px"></td>
+					<td><img src="img8.png" height="50px" width="400px"></td>
 					<td><p>The Cisco 819 Integrated Services Router (ISR) supports machine-to-machine (M2M) applications that can enable enterprises to use 3G wireless WAN network services.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Router: Router-PT</label></td>
-					<td><img src="../img/img9.jpg" height="50px" width="400px"></td>
+					<td><img src="img9.jpg" height="50px" width="400px"></td>
 					<td><p>The Router-PT generic router provides ten slots, one console port, and one auxiliary port.</p></td>
 				</tr>
 			</table>
