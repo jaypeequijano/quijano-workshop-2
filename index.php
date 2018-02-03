0008628a87521200f104b63bb9137750bfed479b
jaypeequijano <35474664+jaypeequijano@users.noreply.github.com>
2018-02-03 11:53:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 </head>
 <body>
 	<label id="header">Cisco Icons and Descriptions</label><br>
-	<a href="php/1.php">Routers</a>
-	<a href="php/2.php">Switches</a>
-	<a href="php/3.php">Other Devices</a>
-	<a href="php/4.php">Cable Types</a>
+	<a href="1.php">Routers</a>
+	<a href="2.php">Switches</a>
+	<a href="3.php">Other Devices</a>
+	<a href="4.php">Cable Types</a>
 </body>
 </html>
