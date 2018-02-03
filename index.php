0ef2457401e8eb142db3da32060a0eecd00eef54
jaypeequijano <35474664+jaypeequijano@users.noreply.github.com>
2018-02-03 13:05:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <html>
 <head>
 	<title> Quijano-Workshop 2</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<label class="headers">Cisco Icons and Descriptions</label><br>
-	<a class="linker" href="php/1.php">Routers</a>
-	<a class="linker"  href="php/2.php">Switches</a>
-	<a class="linker" href="php/3.php">Other Devices</a>
-	<a class="linker" href="php/4.php">Cable Types</a>
+	<a class="linker" href="1.php">Routers</a>
+	<a class="linker"  href="2.php">Switches</a>
+	<a class="linker" href="3.php">Other Devices</a>
+	<a class="linker" href="4.php">Cable Types</a>
 </body>
 </html>
