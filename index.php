92189dcd9a9ed8e8a7988e42b96f5674f4da4a2e
jaypeequijano <35474664+jaypeequijano@users.noreply.github.com>
2018-02-03 11:49:44 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Quijano-Workshop 2</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<label id="header">Cisco Icons and Descriptions</label><br>
+	<a href="php/1.php">Routers</a>
+	<a href="php/2.php">Switches</a>
+	<a href="php/3.php">Other Devices</a>
+	<a href="php/4.php">Cable Types</a>
+</body>
+</html>
