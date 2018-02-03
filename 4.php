01b988b33fdeb7012be4b6ae1607fa23d5a2f02d
jaypeequijano <35474664+jaypeequijano@users.noreply.github.com>
2018-02-03 11:59:22 +0800

    Update 4.php

--- 4.php
+++ 4.php
@@ -17,19 +17,19 @@
 			</tr>
 			<tr>
 					<td><label class="iconName">RJ-45</label></td>
-					<td><img src="../img/27.jpg" height="100px" width="400px"></td>
+					<td><img src="27.jpg" height="100px" width="400px"></td>
 					<td><p>An 8-pin/8-position plug or jack is commonly used to connect computers onto Ethernet-based local area networks (LAN).<br>
 					<br>
 					Two wiring schemes–T568A and T568B–are used to terminate the twisted-pair cable onto the connector interface.</p></td>
 			</tr>
 			<tr>
 					<td><label class="iconName">Straight Through Cable</label></td>
-					<td><img src="../img/28.jpg" height="100px" width="400px"></td>
+					<td><img src="28.jpg" height="100px" width="400px"></td>
 					<td><p>is a type of twisted pair copper wire cable for local area network (LAN) use for which the RJ-45 connectors at each end have the same pinout (i.e., arrangement of conductors).</p></td>
 			</tr>
 			<tr>
 					<td><label class="iconName">Crossover Cable</label></td>
-					<td><img src="../img/29.jpg" height="200px" width="300px"></td>
+					<td><img src="29.jpg" height="200px" width="300px"></td>
 					<td><p>is a type of twisted pair copper wire cable for LANs (local area network) in which the wires on the cable are crossed over so that the receive signal pins on the RJ-45 connector on one end are connected to the transmit signal pins on the RJ-45 connector on the other end.</p></td>
 			</tr>
 			</table>
