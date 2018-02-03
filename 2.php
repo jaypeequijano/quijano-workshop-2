f650163a7b198526596ec3aa497acc721fa29014
jaypeequijano <35474664+jaypeequijano@users.noreply.github.com>
2018-02-03 13:07:12 +0800

    Update 2.php

--- 2.php
+++ 2.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title></title>
-		<link rel="stylesheet" type="text/css" href="../css/style.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 <label class="headers">Cisco Icons and Descriptions</label><br>
@@ -16,40 +16,40 @@
 				</tr>
 				<tr>
 					<td width="150px"><label class="iconName">Switch: 2950-24</label></td>
-					<td><img src="../img/img10.jpg" height="50px" width="400px"></td>
+					<td><img src="img10.jpg" height="50px" width="400px"></td>
 					<td><p>The Cisco Catalyst 2950-24 is a member of the Cisco Catalyst 2950 series switch family. It is a standalone, fixed-configuration, managed 10/100 switch providing user connectivity for small- to mid-sized networks.<br>
 					<br>
 					It does not support add-in modules.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Switch: 2950T-24</label></td>
-					<td><img src="../img/img11.jpg" height="50px" width="400px"></td>
+					<td><img src="img11.jpg" height="50px" width="400px"></td>
 					<td><p>The Cisco Catalyst 2950T-24 is a member of the Catalyst 2950 Series Intelligent Ethernet Switch family. It is a fixed-configuration, standalone switch that provides wire-speed 			Fast Ethernet and Gigabit Ethernet connectivity for mid-sized networks.<br>
 					<br>
 					It does not support add-in modules.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Switch: 2960-24TT</label></td>
-					<td><img src="../img/img12.jpg" height="50px" width="400px"></td>
+					<td><img src="img12.jpg" height="50px" width="400px"></td>
 					<td><p>The Cisco Catalyst 2960-24TT is a member of the Catalyst 2960 Series Intelligent Ethernet Switch family. It is a fixed-configuration, standalone switch that provides wire-speed Fast Ethernet and Gigabit Ethernet connectivity for mid-sized networks.<br>
 					<br>
 					It does not support add-in modules.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Switch: Switch-PT</label></td>
-					<td><img src="../img/img13.jpg" height="50px" width="400px"></td>
+					<td><img src="img13.jpg" height="50px" width="400px"></td>
 					<td><p>The Switch-PT generic switch provides ten slots, one console port, and one auxiliary port.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Switch: 3560-24PS</label></td>
-					<td><img src="../img/img14.jpg" height="50px" width="400px"></td>
+					<td><img src="img14.jpg" height="50px" width="400px"></td>
 					<td><p>The Cisco Catalyst 3560-24PS is a member of the Catalyst 3560 Series Intelligent Ethernet Switch family. It is a fixed-configuration, standalone switch that provides wire-speed Fast Ethernet and Gigabit Ethernet connectivity for mid-sized networks.<br>
 					<br>
 					It does not support add-in modules.</p></td>
 				</tr>
 				<tr>
 					<td><label class="iconName">Bridge-PT</label></td>
-					<td><img src="../img/img15.jpg" height="50px" width="400px"></td>
+					<td><img src="img15.jpg" height="50px" width="400px"></td>
 					<td><p>This bridge provides two slots.<br>
 				<br>
 					The bridge supports the same modules that the Switch-PT supports.</p></td>
